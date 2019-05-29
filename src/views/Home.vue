dd04e090701d86d50833f6498f170bb10deca10a
sunyk <649813494@qq.com>
2019-05-29 13:27:43 +0800

    更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,41 +5,52 @@
         <div class="system-title">后台管理系统</div>
       </el-header>
       <el-container>
-        <el-aside width="200px">
+        <el-aside width="100px">
+          <!--          <el-menu-->
+          <!--            class="el-menu-vertical-demo"-->
+          <!--            @open="handleOpen"-->
+          <!--            :router="true"-->
+          <!--            @close="handleClose"-->
+          <!--            background-color="#545c64"-->
+          <!--            text-color="#fff"-->
+          <!--            active-text-color="#fff"-->
+          <!--          >-->
+
+          <!--            <el-menu-item index="/user">-->
+          <!--              <span >学员列表</span>-->
+          <!--            </el-menu-item>-->
+          <!--            <el-menu-item index="/advertising">-->
+          <!--              <span >广告设置</span>-->
+          <!--            </el-menu-item>-->
+          <!--          </el-menu>-->
+
           <el-menu
-            
+            :default-active="activeIndex"
             class="el-menu-vertical-demo"
-            @open="handleOpen"
-            :router="true"
-            @close="handleClose"
             background-color="#545c64"
             text-color="#fff"
             active-text-color="#fff"
-          >
-           
+            :router="true">
             <el-menu-item index="/user">
-              <span >学员列表</span>
+              <!--              <i class="el-icon-menu"></i>-->
+              <span slot="title">学员列表</span>
+
             </el-menu-item>
             <el-menu-item index="/advertising">
-              <span >广告设置</span>
+              <!--              <i class="el-icon-picture"></i>-->
+              <span slot="title">广告设置</span>
             </el-menu-item>
           </el-menu>
-          
-             <div class="box1 ">
-               <img src="../assets/未登录-头像.png" alt="">
-              
-             </div>
-              <span>
-                 您好
-               </span>
-             <div >
-            <div class="box2" >
-              <img src="../assets/开关 (1).png" alt="">
-            </div>
+          <div class="aside-bottom">
+            <img src="../assets/header.png" class="header-icon" alt="">
+            <br>
+            <span class="user-name">admin</span>
+            <br>
+            <img src="../assets/loginout.png" class="loginout-icon" alt="">
           </div>
         </el-aside>
         <el-main>
-        <router-view></router-view>
+          <router-view></router-view>
         </el-main>
       </el-container>
     </el-container>
@@ -47,75 +58,105 @@
 </template>
 
 <script>
-export default {
-  name: "",
-  data() {
-    return {};
-  }
-};
+  export default {
+    name: "",
+    data() {
+      return {};
+    },
+    computed: {
+      activeIndex() {
+        return this.$route.path;
+      }
+    }
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-body{
-  padding: 0;
-  margin: 0;
-}
-.el-main{
-  background-color:rgb(246, 248, 250);
-}
-.home {
-  height: 100%;
-   .el-menu-admin:not(.el-menu--collapse) {
-    width: 200px;
-    min-height: 400px;
+  body {
+    padding: 0;
+    margin: 0;
   }
-}
-.system-title {
-  font-size: 16px;
-padding-left: 180px;
-  color: #fff;
-}
-.el-container{
-  height: 100%;
-}
- .el-aside {
+
+  .el-main {
+    background-color: rgb(246, 248, 250);
+    min-width: 800px;
+  }
+
+  .home {
+    height: 100%;
+
+    .el-menu-admin:not(.el-menu--collapse) {
+      width: 200px;
+      min-height: 400px;
+    }
+  }
+
+  .system-title {
+    font-size: 12px;
+    /*padding-left: 180px;*/
+    color: #fff;
+  }
+
+  .el-container {
+    height: 100%;
+  }
+
+  .el-aside {
     background-color: #545c64;
     position: relative;
+    /*padding-top:60px;*/
   }
-.el-header {
-  background-color: rgb(89, 142, 202);
-  height: 40px;
-  display: flex;
-  justify-content: baseline;
-  align-items: center;
-  padding-left: 30px;
-  background-color: #545c64;
-}
- .home .el-menu-admin:not(.el-menu--collapse) {
+
+  .el-header {
+    height: 40px;
+    display: flex;
+    justify-content: baseline;
+    align-items: center;
+    padding-left: 20px;
+    background: linear-gradient(90deg, rgba(61, 149, 230, 1) 0%, rgba(51, 210, 231, 1) 100%);
+  }
+
+  .home .el-menu-admin:not(.el-menu--collapse) {
     width: 200px;
     min-height: 400px;
   }
-  .box2{
-    width: 100%;
-    display: flex;
-     justify-content:center;
-     position: absolute;
-     bottom: 50px;
+
+  .el-menu-item.is-active {
+    background: rgba(60, 156, 230, 1) !important;
   }
-  .box2 img{
-    width: 30px;
-  height: 30px;
-}
-.box1{
-  width: 100%;
-    display: flex;
-     justify-content:center;
-     position: absolute;
-     bottom: 100px;
-}
- .box1 img{
-    width: 40px;
-  height: 40px;
-}
+
+  .el-menu-item {
+    font-size: 14px;
+    height: 40px !important;
+    line-height: 40px !important;
+  }
+
+  .el-menu {
+    border-right: none !important;
+    padding-top: 30px;
+  }
+
+  .header-icon {
+    width: 25px;
+    margin-bottom: 5px;
+
+  }
+
+  .user-name {
+    color: #fff;
+    font-size: 12px;
+  }
+
+  .loginout-icon {
+    width: 28px;
+    margin-top: 65px;
+  }
+
+  .aside-bottom {
+    position: absolute;
+    bottom: 25px;
+    text-align: center;
+    width: 100%;
+  }
 </style>
--- src/views/advertisingset/AdvertisingSet.vue
+++ src/views/advertisingset/AdvertisingSet.vue
@@ -2,31 +2,64 @@
   <div>
     <el-tabs type="border-card">
       <el-tab-pane label="宣传链接广告位图片">
-        <div class="box">
-          <p>广告示例照片:</p>
-          <img src="../../assets/微信图片_20190527120156.png" alt>
-        </div>
-        <div class="box2">
-          <span class="p2">请上传广告图片:</span>
-            <el-upload
-              class="avatar-uploader"
-              action="https://jsonplaceholder.typicode.com/posts/"
-              :show-file-list="false"
-              :on-success="handleAvatarSuccess"
-              :before-upload="beforeAvatarUpload"
-            >
-              <img v-if="imageUrl" :src="imageUrl" class="avatar">
-              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-            </el-upload>
-        </div>
-        <div class="button">
-          <el-button type="primary">提交</el-button>
+        <div class="content">
+          <div class="box">
+            <span class="label">广告示例照片:</span>
+            <div class="right-box">
+            <img src="../../assets/example.png" alt>
+            </div>
+          </div>
+          <div class="box2">
+            <span class="label">请上传广告图片:</span>
+            <div class="right-box upload-right">
+              <el-upload
+                class="avatar-uploader"
+                action="https://jsonplaceholder.typicode.com/posts/"
+                :show-file-list="false"
+                :on-success="handleAvatarSuccess"
+                :before-upload="beforeAvatarUpload"
+              >
+                <img v-if="imageUrl" :src="imageUrl" class="avatar">
+                <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+              </el-upload>
+            </div>
+          </div>
+          <div class="button">
+            <el-button type="primary" size="small">提交</el-button>
+          </div>
         </div>
       </el-tab-pane>
-      <el-tab-pane label="已参与用户的奖励提示">配置管理</el-tab-pane>
+      <el-tab-pane label="已参与用户的奖励提示">
+        <div class="content">
+          <div class="box">
+            <span class="label">奖励提示示例图片:</span>
+            <div class="right-box">
+              <img src="../../assets/example.png" alt>
+            </div>
+          </div>
+          <div class="box2">
+            <span class="label">请上传奖励提示图片:</span>
+            <div class="right-box upload-right">
+              <el-upload
+                class="avatar-uploader"
+                action="https://jsonplaceholder.typicode.com/posts/"
+                :show-file-list="false"
+                :on-success="handleAvatarSuccess"
+                :before-upload="beforeAvatarUpload"
+              >
+                <img v-if="imageUrl" :src="imageUrl" class="avatar">
+                <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+              </el-upload>
+            </div>
+          </div>
+          <div class="button">
+            <el-button type="primary" size="small">提交</el-button>
+          </div>
+        </div>
+      </el-tab-pane>
       <el-tab-pane label="音乐设置">
-         <div class="box3">
-
+        <div class="content">
+          <div class="box3">
             <el-upload
               class="avatar-uploader"
               action="https://jsonplaceholder.typicode.com/posts/"
@@ -37,99 +70,138 @@
               <img v-if="imageUrl" :src="imageUrl" class="avatar">
               <i v-else class="el-icon-plus avatar-uploader-icon"></i>
             </el-upload>
-            <span style="margin-left:15px;">点击上传MP3格式的音乐，不能超过5M</span>
+            <span class="tip">点击上传MP3格式的音乐，不能超过5M</span>
+          </div>
+
         </div>
+
       </el-tab-pane>
     </el-tabs>
   </div>
 </template>
 <script>
-export default {
-  data() {
-    return {
-      imageUrl: ""
-    };
-  },
-  methods: {
-    handleAvatarSuccess(res, file) {
-      this.imageUrl = URL.createObjectURL(file.raw);
+  export default {
+    data() {
+      return {
+        imageUrl: ""
+      };
     },
-    beforeAvatarUpload(file) {
-      const isJPG = file.type === "image/jpeg";
-      const isLt2M = file.size / 1024 / 1024 < 2;
+    methods: {
+      handleAvatarSuccess(res, file) {
+        this.imageUrl = URL.createObjectURL(file.raw);
+      },
+      beforeAvatarUpload(file) {
+        const isJPG = file.type === "image/jpeg";
+        const isLt2M = file.size / 1024 / 1024 < 2;
 
-      if (!isJPG) {
-        this.$message.error("上传头像图片只能是 JPG 格式!");
+        if (!isJPG) {
+          this.$message.error("上传头像图片只能是 JPG 格式!");
+        }
+        if (!isLt2M) {
+          this.$message.error("上传头像图片大小不能超过 2MB!");
+        }
+        return isJPG && isLt2M;
       }
-      if (!isLt2M) {
-        this.$message.error("上传头像图片大小不能超过 2MB!");
-      }
-      return isJPG && isLt2M;
     }
-  }
-};
+  };
 </script>
 <style lang="scss" scoped>
-.box {
-  width: 100%;
-  /* display: flex;
-  justify-content: center;
-  align-items: center; */
-  padding-left: 299px;
-}
-.box p {
-  display: inline-block;
-  margin-right: 30px;
-}
-.p2 {
-  padding-right: 15px;
-}
-.box2 {
-  padding: 50px 299px;
-  width: 100%;
-  display: flex;
-  align-items: center;
-}
-.el-upload--picture-card {
-  width: 80px;
-  height: 80px;
-  line-height: 80px;
-}
-.avatar-uploader{
-     border: 1px dashed #d9d9d9;
-}
-.avatar-uploader .el-upload {
-  border: 1px dashed #d9d9d9;
-  border-radius: 6px;
-  cursor: pointer;
-  position: relative;
-  overflow: hidden;
-}
-.avatar-uploader .el-upload:hover {
-  border-color: #409eff;
-}
-.avatar-uploader-icon {
-  font-size: 28px;
-  color: #8c939d;
-  width: 78px;
-  height: 78px;
-  line-height: 78px;
-  text-align: center;
-}
-.avatar {
-  width: 78px;
-  height: 78px;
-  display: block;
-}
-.button {
-  display: flex;
-  justify-content: center;
-  margin-bottom: 150px;
-}
-.box3 {
-   padding: 50px 299px;
-  width: 100%;
-  display: flex;
-  align-items:flex-end;
-}
+  .box {
+    width: 100%;
+    padding-bottom:30px;
+    /* display: flex;
+    justify-content: center;
+    align-items: center; */
+    /*padding-left: 299px;*/
+  }
+
+  .content {
+    padding: 30px 40px;
+    min-height: 500px;
+  }
+
+  .label {
+    font-size: 14px;
+    color: #333;
+    width: 150px;
+    display: inline-block;
+  }
+  .right-box{
+    display: inline-block;
+    vertical-align: top;
+  }
+  .upload-right{
+    padding-left:7px;
+  }
+  .tip{
+    font-size:14px;
+    margin-left:15px;
+  }
+
+
+    /*.box p {*/
+  /*  display: inline-block;*/
+  /*  margin-right: 30px;*/
+  /*}*/
+
+  /*.p2 {*/
+  /*  padding-right: 15px;*/
+  /*}*/
+
+  /*.box2 {*/
+  /*  padding: 50px 0;*/
+  /*  width: 100%;*/
+  /*  display: flex;*/
+  /*  align-items: center;*/
+  /*}*/
+
+  .el-upload--picture-card {
+    width: 80px;
+    height: 80px;
+    line-height: 80px;
+  }
+
+  .avatar-uploader {
+    display: inline-block;
+    border: 1px dashed #d9d9d9;
+  }
+
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+
+  .avatar-uploader .el-upload:hover {
+    border-color: #409eff;
+  }
+
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 78px;
+    height: 78px;
+    line-height: 78px;
+    text-align: center;
+  }
+
+  .avatar {
+    width: 78px;
+    height: 78px;
+    display: block;
+  }
+
+  .button {
+    margin:70px 150px 50px 150px;
+
+  }
+
+  /*.box3 {*/
+  /*  padding: 50px 299px;*/
+  /*  width: 100%;*/
+  /*  display: flex;*/
+  /*  align-items: flex-end;*/
+  /*}*/
 </style>
